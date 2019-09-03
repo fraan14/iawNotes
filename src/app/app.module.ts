375555fc5c2e3ee002ef53669eb06b75239b2557
franco <fraanco.st@gmail.com>
2019-09-03 14:29:36 -0300

    nueva navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {MaterialModule} from './material.module'
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout'
 
 import { AppComponent } from './app.component';
+import { fromEventPattern } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import{MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatSidenav, MatIcon} from '@angular/material';
+import { fromEventPattern } from 'rxjs';
+
+@NgModule({
+    imports:[
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule
+    ],
+    exports:[
+        MatSidenavModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule
+    ]
+})
+
+export class MaterialModule{}
