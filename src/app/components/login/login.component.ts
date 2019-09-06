6f84030cf330b5349160b63a4b1d4c0970c47afe
franco <fraanco.st@gmail.com>
2019-09-06 14:59:06 -0300

    asd

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
+import {MatDialogRef} from '@angular/material'
 import {auth} from 'firebase/app';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,10 @@ import {auth} from 'firebase/app';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public afAuth:AngularFireAuth) { }
+  constructor(public afAuth:AngularFireAuth, private dialogRef: MatDialogRef<LoginComponent>,private authService:AuthService) { }
+
+  public email:string =""; //asd
+  public pass:string="";
 
   ngOnInit() {
   }
@@ -20,7 +25,35 @@ export class LoginComponent implements OnInit {
   CancelarLogin(){
   }
 
-  onLoginGoogle(){
-    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  onLogin():void{
+    this.authService.loginEmailUser(this.email,this.pass).then((res)=>{
+      this.close()
+    }).catch(err => console.log('err',err.message));
   }
+
+  onLoginGoogle():void{
+    //this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    this.authService.loginGoogleUser().then((res)=>{
+      this.close();
+    }).catch(err=>{
+      console.log('error',err.message);   //esto se ejecuta cuando sucede un error, aca tendria que evitar el cierre del popup y poner en rojo el problema
+    })
+  }
+  onLoginFacebook(){
+    //this.afAuth.auth.signInWithPopup( new auth.FacebookAuthProvider());  
+    this.authService.loginFacebookUser().then((res)=>{
+      this.close();
+    }).catch(err=>{
+      console.log('error',err.message);   //esto se ejecuta cuando sucede un error, aca tendria que evitar el cierre del popup y poner en rojo el problema
+    })
+  }
+
+  onLogout(){
+    this.authService.logoutUser();
+  }
+
+  close() {
+    this.dialogRef.close();
+}
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
+import {auth} from 'firebase/app';
 
 //en este servicio esta todo lo relacionado con el login y registro de usuarios
 
@@ -7,12 +10,28 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afsAuth:AngularFireAuth) { }
 
   registerUser(){ }
-  loginEmailUser(){ }
-  loginFacebookUser(){ }
-  loginGoogleUser(){ }
-  logoutUser(){ }
+  loginEmailUser(email:string , pass:string){
+
+    return new Promise((resolve,reject)=>{
+      this.afsAuth.auth.signInWithEmailAndPassword(email,pass).then(userData=>resolve(userData),
+      err=> reject(err));
+    });
+
+   }
+  loginFacebookUser(){ 
+    return this.afsAuth.auth.signInWithPopup( new auth.FacebookAuthProvider()); 
+  }
+  loginGoogleUser(){ 
+    return this.afsAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()); }
+  logoutUser(){
+    return this.afsAuth.auth.signOut();
+   }
+
+  isAuth(){
+    return this.afsAuth.authState.pipe(map(auth=>auth));
+  }
 
 }
